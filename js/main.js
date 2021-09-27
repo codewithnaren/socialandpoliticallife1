2624390cfcc48fcf1a0787c827baec3b544f1eae
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-09-27 20:55:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
 
 
 let pdfDoc = null,
